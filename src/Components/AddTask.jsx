2facf7bf88e9a12736a231394f962337a7864731
Pavel <pavol.prcin@gmail.com>
2021-03-25 16:02:23 +0100

    first commit

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -3,40 +3,58 @@ import { React, useState } from 'react';
 
 const AddTask = ({ onAdd }) => {
 
-    const [text, setText] = useState('');
-    const [day, setDate] = useState('');
+    const [text, setFilm] = useState('');
+    // const [day, setDate] = useState({});
     const [reminder, setReminder] = useState(true);
+    const [rating, setRating] = useState('');
+    const [duratation, setDuratation] = useState('');
+    const [type, setType] = useState('');
 
     const onSubmit = (e) => {
         e.preventDefault()
 
-        if (!text) {
+        if (!(text && rating && duratation && type)) {
             alert('Please add text')
             return
         }
+        // setDate({ currentTime: new Date().toLocaleString() })
 
-        onAdd({ text, day, reminder })
+        onAdd({ text, rating, duratation, type, reminder })
 
-        setText('');
-        setDate('');
+        setFilm('');
+        setRating('');
+        setDuratation('');
+        //setDate('');
+        setType('');
         setReminder(false);
     }
     return (
         <div>
             <form className='add-form' onSubmit={onSubmit}>
                 <div className='form-control'>
-                    <label>Task</label>
-                    <input type='text' placeholder='addTask' value={text} onChange={(e) => setText(e.target.value)}></input>
+                    <label>Film</label>
+                    <input type='text' placeholder='addFilm' value={text} onChange={(e) => setFilm(e.target.value)}></input>
                 </div>
                 <div className='form-control'>
-                    <label>Time and day</label>
-                    <input type='text' placeholder='add time and day' value={day} onChange={(e) => setDate(e.target.value)}></input>
+                    <label>Rating</label>
+                    <input type='text' placeholder='addRation' value={rating} onChange={(e) => setRating(e.target.value)}></input>
+                </div>
+                <div className='form-control'>
+                    <label>Duratation</label>
+                    <input type='text' placeholder='addDuratation' value={duratation} onChange={(e) => setDuratation(e.target.value)}></input>
+                </div>
+                <div className='form-control'>
+                    <label>Type of movie</label>
+                    <input type='text' placeholder='add type of movie' value={type} onChange={(e) => setType(e.target.value)}></input>
                 </div>
                 <div className='form-control form-control-check'>
                     <label>Set reminder</label>
                     <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} checked={reminder}></input>
                 </div>
-
+                {/* <div className='form-control'>
+                    <label>Time and day</label>
+                    <input type='text' placeholder='add time and day' value={day} onChange={(e) => setDate(e.target.value)}></input>
+                </div> */}
                 <input type='submit' className='btn btn-block' value='Save task'></input>
             </form>
         </div>
--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
+    const [date, setDate] = useState((new Date().toLocaleDateString()));
+
+
     return (
-        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
-            <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} /></h3>
-            <p>{task.day}</p>
+        <div className='table-container'>
+            <table className='table'>
+                <tbody>
+                    <tr className='table-movie-titles'>
+                        <th>Film</th>
+                        <th>Rating</th>
+                        <th>Duratation</th>
+                        <th>Type</th>
+                        <th>Date and time</th>
+                    </tr>
+                    <tr className='table-movie-values'>
+                        <td>{task.text}</td>
+                        <td>{task.rating}</td>
+                        <td>{task.duratation}</td>
+                        <td>{task.type}</td>
+                        <td>{date}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <h3><FaTimes style={{ color: 'red', cursor: 'pointer', marginTop: '32px', fontSize: '20px', marginLeft: "5px" }} onClick={() => onDelete(task.id)} /></h3>
         </div>
     )
 }
